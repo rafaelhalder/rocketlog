63f4fcaf1ce91e4a0af3a8da64b2c2c5061f575b
Rafael Silva <rafael.silva@tecnorisk.com.br>
2025-03-10 17:21:45 -0300

    Adds status update endpoint for deliveries
    Introduces a new PATCH endpoint to update delivery statuses.
    Includes user authentication and authorization middleware.

---
+++ src/controllers/deliveries-status-controller.ts
@@ -0,0 +1,31 @@
+import { Request,Response,NextFunction } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
+
+class DeliveriesStatusController{
+  async update(request: Request, response: Response, next: NextFunction) {
+   
+    const paramsSchema = z.object({
+      id: z.string().uuid()
+    })
+
+    const bodySchema = z.object({
+      status: z.enum(["processing","shipped","delivered"])
+    })
+
+    const {id} = paramsSchema.parse(request.params)
+    const {status} = bodySchema.parse(request.body)
+
+    const delivery = await prisma.delivery.update({
+      where:{
+        id
+      },
+      data:{
+        status
+      }
+    })
+
+    return response.json();
+  }
+}
+export {DeliveriesStatusController}
--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -2,9 +2,12 @@ import { Router } from "express";
 import { DeliveriesController } from "@/controllers/deliveries-controller";
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization";
+import { DeliveriesStatusController } from "@/controllers/deliveries-status-controller";
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
+const deliveriesStatusController = new DeliveriesStatusController();
 deliveriesRoutes.use(ensureAuthenticated,verifyUserAuthorization(["sale"]));
+deliveriesRoutes.patch("/:id/status", deliveriesStatusController.update);
 deliveriesRoutes.post("/", deliveriesController.create);
 
 export {deliveriesRoutes}
